602e1750b9f988cdbc3eb49f7491cb5eb7d020d2
Quezadajl <guicho.21.jq@gmail.com>
2022-01-18 12:34:33 -0600

    exercises

--- Notebook/stackexchange.sql
+++ Notebook/stackexchange.sql
@@ -1,10 +1,8 @@
 SELECT *
 FROM stackexchange;
-
 /***count the number of columns of non-NULL values with DISTINCT*/
 SELECT COUNT(DISTINCT tag)
 FROM stackexchange;
-
 /***Below you can use INFORMATION_SCHEMA to count the columns in a table ***/
 SELECT COUNT(*)
 FROM INFORMATION_SCHEMA.COLUMNS
@@ -18,4 +16,13 @@ ORDER BY count DESC;
 SELECT question_count
 FROM stackexchange
 WHERE question_count IS NOT NULL;
-/****/
+/**Numeric Data types & Summary functions**/
+SELECT max(question_pct) --also min; avg
+FROM stackexchange;
+/**variance and Standard Deviation*/
+SELECT var_pop(question_pct) 
+FROM stackexchange;
+---AND----
+SELECT round(stddev(question_pct),5)
+FROM stackexchange;
+
