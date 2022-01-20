f9ec6cc95b6acadda684d9f05ce784972d94da78
Quezadajl <guicho.21.jq@gmail.com>
2022-01-19 18:20:46 -0600

    exercises

--- Notebook/stackexchange.sql
+++ Notebook/stackexchange.sql
@@ -86,5 +86,22 @@ WHERE tag='dropbox';
 SELECT generate_series(2200,3050,50) AS lower,
 	generate_series(2250,3100,50) AS upper,
 -----Generate Series: step 3-----
+WITH bins AS (
+	SELECT generate_series(2200,3050,50) AS lower,
+	generate_series(2250,3100,50) AS upper),
+	---subset stackoverflow to just tag dropbox---
+	dropbox AS (
+	SELECT question_count
+	FROM stackexchange
+	WHERE tag = 'dropbox')
+	----select columns for results---
+SELECT lower, upper, COUNT(question_count)
+FROM bins
+JOIN dropbox
+ON question_count >= lower
+AND question_count < upper
+GROUP BY lower, upper
+ORDER BY lower;
+
 
 	
