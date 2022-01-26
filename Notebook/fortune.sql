4e02fdd0311fdb68a7661504fd4d156fb382d431
Quezadajl <guicho.21.jq@gmail.com>
2022-01-25 18:58:53 -0600

    exercises

--- Notebook/fortune.sql
+++ Notebook/fortune.sql
@@ -127,4 +127,33 @@ FROM fortune
 LEFT JOIN profit80
 ON fortune.sector = profit80.sector
 WHERE profits > pct80;
------
+---Summary of the entire chapter- correlations, casting, TEMP Tables and JOINS--
+DROP TABLE IF EXISTS correlations;
+
+CREATE TEMP TABLE correlations AS
+SELECT 'profits'::varchar AS measure,
+       corr(profits, profits) AS profits,
+       corr(profits, profits_change) AS profits_change,
+       corr(profits, revenues_change) AS revenues_change
+  FROM fortune;
+
+INSERT INTO correlations
+SELECT 'profits_change'::varchar AS measure,
+       corr(profits_change, profits) AS profits,
+       corr(profits_change, profits_change) AS profits_change,
+       corr(profits_change, revenues_change) AS revenues_change
+  FROM fortune;
+
+INSERT INTO correlations
+SELECT 'revenues_change'::varchar AS measure,
+       corr(revenues_change, profits) AS profits,
+       corr(revenues_change, profits_change) AS profits_change,
+       corr(revenues_change, revenues_change) AS revenues_change
+  FROM fortune;
+
+-- Select each column, rounding the correlations
+SELECT measure, 
+       round(profits::numeric, 2) AS profits,
+       round(profits_change::numeric, 2) AS profits_change,
+       round(revenues_change::numeric, 2) AS revenues_change
+  FROM correlations;
