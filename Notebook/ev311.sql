cfa8defcd11922e8898d175a7af9c6743377cf1f
Quezadajl <guicho.21.jq@gmail.com>
2022-02-01 18:13:50 -0600

    exercises

--- Notebook/ev311.sql
+++ Notebook/ev311.sql
@@ -118,3 +118,8 @@ GROUP BY priority;
 SELECT COUNT(*)
 FROM ev311
 WHERE date_created::date = '2017-01-31';
+---OR---
+SELECT COUNT(*)
+FROM ev311
+WHERE date_created >= '2016-02-29'
+AND date_created < '2016-03-01';
