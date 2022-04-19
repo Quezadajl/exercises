0851a74a40b4c672cddb68a948627362abd892e1
Quezadajl <guicho.21.jq@gmail.com>
2022-04-19 17:28:00 -0500

    exercises

--- Notebook/DVDRentals.sql
+++ Notebook/DVDRentals.sql
@@ -1,1 +1,8 @@
-SELECT * FROM actor;
+--Introductory query--
+SELECT * FROM category;
+/*SELECT title, length
+FROM film AS f
+INNER JOIN category AS c
+ON f.film_id = c.film_id
+WHERE c.catergory = 'Documentary' AND f.rating IN ('G','PG')
+ORDER BY length DESC;*/
