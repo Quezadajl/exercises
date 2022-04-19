0da18b2aed2019380ba94ff2d16877b0c89bc656
Quezadajl <guicho.21.jq@gmail.com>
2022-04-19 17:57:31 -0500

    exercises

--- Notebook/DVDRentals.sql
+++ Notebook/DVDRentals.sql
@@ -8,6 +8,16 @@ ON f.film_id = c.category_id
 WHERE c.name = 'Documentary' AND f.rating IN ('G','PG')
 ORDER BY length DESC;
 ------Review the essentials-----------------
-SELECT COUNT(DISTINCT(language_id))
-FROM film
-;
+SELECT title, description
+FROM film AS f
+INNER JOIN language AS l
+ON f.language_id = l.language_id
+WHERE l.name = 'English'
+AND f.release_year = 2006;
+-------Practice essentials------------
+SELECT first_name, last_name, amount
+FROM payment AS p
+INNER JOIN customer AS c
+ON p.customer_id = c.customer_id
+WHERE c.activebool = true
+ORDER BY amount DESC;
