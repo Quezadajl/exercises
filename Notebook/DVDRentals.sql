d52f445d78f331615b53f714be415310b7d8b767
Quezadajl <guicho.21.jq@gmail.com>
2022-04-19 17:43:20 -0500

    exercises

--- Notebook/DVDRentals.sql
+++ Notebook/DVDRentals.sql
@@ -8,11 +8,7 @@ ON f.film_id = c.category_id
 WHERE c.name = 'Documentary' AND f.rating IN ('G','PG')
 ORDER BY length DESC;
 ------Review the essentials-----------------
-/*SELECT title, description
+SELECT title, description
 FROM film as f
 INNER JOIN language AS l
-ON f.language_id = l.language_id
-WHERE l.name IN ('Italian','French')
-AND f.release_year = '2006';*/
-SELECT title, description
-FROM film;
+ON f.language_id = l.language_id;
