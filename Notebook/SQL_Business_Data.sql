c722c486f5f39b1516a49ce24279ee67c56f2779
Quezadajl <guicho.21.jq@gmail.com>
2022-04-27 18:03:41 -0500

    exercises

--- Notebook/SQL_Business_Data.sql
+++ Notebook/SQL_Business_Data.sql
@@ -1,9 +1,13 @@
 
-SELECT f.film_id,
-	rental_rate * COUNT(customer_id) AS part
+WITH temp_table AS (SELECT f.film_id,
+	rental_rate * COUNT(customer_id) AS pre
 FROM film AS f
 INNER JOIN inventory AS inv
 	ON f.film_id = inv.film_id
 INNER JOIN rental AS r
 	ON inv.inventory_id = r.inventory_id
-GROUP BY f.film_id;
+GROUP BY f.film_id)
+
+SELECT film_id, SUM(pre) AS Revenue
+FROM temp_table
+GROUP BY film_id;
